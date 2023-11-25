67ec5e89f1367601b33007b1e655c52b7d2d7745
WindHead <natanaelgbrandt@gmail.com>
2023-11-24 23:16:10 -0300

    mano, essa foi ferrada

--- Coracao.java
+++ Coracao.java
@@ -1,37 +1,29 @@
 import java.util.Scanner;
 public class Coracao {
     public static void main(String[] args) {
+        
         System.out.println("Hello Word, eu te amo cara S2.");
         System.out.println("Essa foi fácil");
         System.out.println("então vamos lá com a sequencia de fibonacci");
         Scanner scanner = new Scanner(System.in);
-        int numero1 = 1;
-        int numero2 = 1;
-        int x;
-        int y;
-        int z;
-        System.out.println(numero1);
-        System.out.println(numero2);
-        System.out.println(x = numero1 + numero2);
-        System.out.println(y = x + numero2);
-        System.out.println(z = x + y);
-        System.out.println(x = y + z);
-        System.out.println(y = z + x);
-        System.out.println(z = x + y);
-        System.out.println(x = y + z);
-        System.out.println(y = z + x);
-        System.out.println(z = x + y);
-        System.out.println(x = y + z);
-        System.out.println(y = z + x);
-        System.out.println(z = x + y);
-        System.out.println(x = y + z);
-        System.out.println(y = z + x);
-        System.out.println(z = x + y);
-        System.out.println(x = y + z);
-        System.out.println(y = z + x);
-        System.out.println(z = x + y);
+        System.out.print("digite a quantidade de vezes: ");
+        int x = scanner.nextInt();
+        int a = 0;
+        int b = 1;
+        int c;
+        for (int i = 1; i < x; i++) {
+
+            c = a + b;
+
+            System.out.println(c);
+
+            a = b;
+            b = c;
 
 
-        scanner.close();
+        }     
+        
+
+        scanner.close(); 
     }
 }
